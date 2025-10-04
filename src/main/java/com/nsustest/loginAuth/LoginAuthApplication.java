20bd3f04ca802ee5881484d1e797735320b56121
yoon <yoon@yoonui-MacBookAir.local>
2025-10-04 11:02:48 +0900

    Initial commit: Spring Boot login authentication project

---
+++ src/main/java/com/nsustest/loginAuth/LoginAuthApplication.java
@@ -0,0 +1,15 @@
+package com.nsustest.loginAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthApplication.class, args);
+
+		System.out.println("start test");
+	}
+
+}
